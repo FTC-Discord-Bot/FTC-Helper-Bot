5c8f437d4dfeacc1d373cc2b53d9470cf8d713ab
RCGV1 <Benjaminfaer@gmail.com>
2023-08-19 17:51:03 -0700

    Added logic if commands sent in DMS

--- src/main/java/com/bot/commands/BotCommands.java
+++ src/main/java/com/bot/commands/BotCommands.java
@@ -2179,7 +2179,11 @@ Code X API is not working for the time being so the command is disabled
         long endTime = System.nanoTime();
         long duration = (endTime - startCmnd);
         long durationMilis = TimeUnit.NANOSECONDS.toMillis(duration);
-        System.out.println("\u001B[32m Command took " + durationMilis + " milliseconds, in server: "+event.getGuild().getName()+"\u001B[0m");
+        if (event.getGuild() == null) {
+            System.out.println("\u001B[32m Command took " + durationMilis + " milliseconds, in DMs: " + event.getUser().getName() + "\u001B[0m");
+        } else {
+            System.out.println("\u001B[32m Command took " + durationMilis + " milliseconds, in server: " + event.getGuild().getName() + "\u001B[0m");
+        }
     }
 
     // Methods
